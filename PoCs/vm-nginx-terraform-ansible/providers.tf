e24f6706bacb722c848d8c9bca26cd767b2d4084
Sandro Cicero <devopsvanilla@outlook.com>
2026-08-08 20:30:33 -0300

    feat: add google provider configuration with dynamic credentials and project scoping

--- PoCs/vm-nginx-terraform-ansible/providers.tf
+++ PoCs/vm-nginx-terraform-ansible/providers.tf
@@ -1,22 +1,8 @@
-locals {
-  raw_gcp_credentials = try(trimspace(var.gcp_credentials), "")
-  gcp_credentials_json = (
-    local.raw_gcp_credentials == ""
-    ? null
-    : can(jsondecode(local.raw_gcp_credentials))
-    ? local.raw_gcp_credentials
-    : can(base64decode(local.raw_gcp_credentials))
-    ? base64decode(local.raw_gcp_credentials)
-    : local.raw_gcp_credentials
-  )
-}
-
 provider "google" {
   project               = var.project_id
   region                = var.region
   zone                  = var.zone
   billing_project       = var.project_id
   user_project_override = true
-  credentials           = local.gcp_credentials_json
+  credentials           = var.gcp_credentials != "" ? var.gcp_credentials : null
 }
-
