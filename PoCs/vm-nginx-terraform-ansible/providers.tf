6cd6d02989b03a3e49daaa06c09541dd9fcc7685
Sandro Cicero <devopsvanilla@outlook.com>
2026-08-08 21:28:02 -0300

    fix(gcp): remove billing_project override from providers.tf

--- PoCs/vm-nginx-terraform-ansible/providers.tf
+++ PoCs/vm-nginx-terraform-ansible/providers.tf
@@ -1,8 +1,6 @@
 provider "google" {
-  project               = var.project_id
-  region                = var.region
-  zone                  = var.zone
-  billing_project       = var.project_id
-  user_project_override = true
-  credentials           = var.gcp_credentials != "" ? var.gcp_credentials : null
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+  credentials = var.gcp_credentials != "" ? var.gcp_credentials : null
 }
