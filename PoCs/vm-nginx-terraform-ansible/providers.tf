3d7559588e5b9cf57540afcd4fca55e6eb092f1a
Sandro Cicero <devopsvanilla@outlook.com>
2026-08-10 19:25:46 -0300

    fix(hcl4j): revert providers.tf to clean single-line syntax to eliminate hcl4j parser exception

--- PoCs/vm-nginx-terraform-ansible/providers.tf
+++ PoCs/vm-nginx-terraform-ansible/providers.tf
@@ -2,5 +2,5 @@ provider "google" {
   project     = var.project_id
   region      = var.region
   zone        = var.zone
-  credentials = try(trimspace(var.gcp_credentials), "") != "" ? (can(base64decode(var.gcp_credentials)) && !can(jsondecode(var.gcp_credentials)) ? base64decode(var.gcp_credentials) : var.gcp_credentials) : null
+  credentials = var.gcp_credentials != "" ? var.gcp_credentials : null
 }
