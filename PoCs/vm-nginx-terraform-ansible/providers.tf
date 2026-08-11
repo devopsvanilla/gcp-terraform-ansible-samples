853c24144ac03d8e0d411f450def03a6904609b8
Sandro Cicero <devopsvanilla@outlook.com>
2026-08-11 20:02:00 -0300

    feat(provider): add fail-safe check for /var/opt/morpheus/gcp-key.json local keyfile

--- PoCs/vm-nginx-terraform-ansible/providers.tf
+++ PoCs/vm-nginx-terraform-ansible/providers.tf
@@ -2,5 +2,5 @@ provider "google" {
   project     = var.project_id
   region      = var.region
   zone        = var.zone
-  credentials = var.gcp_credentials != "" && var.gcp_credentials != null ? try(base64decode(var.gcp_credentials), var.gcp_credentials) : null
+  credentials = fileexists("/var/opt/morpheus/gcp-key.json") ? file("/var/opt/morpheus/gcp-key.json") : (var.gcp_credentials != "" && var.gcp_credentials != null ? try(base64decode(var.gcp_credentials), var.gcp_credentials) : null)
 }
