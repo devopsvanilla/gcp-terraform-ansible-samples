e279af151b1b92890a9cb6ae23fa23928a138d15
Sandro Cicero <devopsvanilla@outlook.com>
2026-08-23 10:55:51 -0300

    feat: add Morpheus catalog item resource for native Terraform App Blueprint

--- PoCs/morpheus-tf-nativestate/catalog_item.tf
+++ PoCs/morpheus-tf-nativestate/catalog_item.tf
@@ -13,9 +13,25 @@ resource "hpe_morpheus_catalog_item_app_blueprint" "vm_nginx" {
       id: ${var.morpheus_group_id}
     cloud:
       id: ${var.morpheus_cloud_id}
-    config:
-      terraform:
-        commandOptions: "-var 'vm_name=<%= customOptions.vm_name %>' -var 'name=<%= customOptions.vm_name %>' -var 'machine_series=<%= customOptions.machine_series %>' -var 'machine_type_override=<%= customOptions.machine_type_override %>' -var 'vcpu_count=<%= customOptions.vcpu_count %>' -var 'memory_gb=<%= customOptions.memory_gb %>' -var 'disk_type=<%= customOptions.disk_type %>' -var 'disk_size_gb=<%= customOptions.disk_size_gb %>' -var 'boot_image_project=<%= customOptions.boot_image_project %>' -var 'boot_image_family=<%= customOptions.boot_image_family %>' -var 'assign_external_ip=<%= customOptions.assign_external_ip %>' -var 'ssh_username=<%= customOptions.ssh_username %>' -var 'ssh_public_key=<%= customOptions.ssh_public_key %>' -var 'user_groups=<%= customOptions.user_groups %>' -var 'network_name=<%= customOptions.network_name %>' -var 'subnetwork_name=<%= customOptions.subnetwork_name %>' -var 'allowed_http_cidr=<%= customOptions.allowed_http_cidr %>' -var 'allowed_ssh_cidr=<%= customOptions.allowed_ssh_cidr %>'"
+    templateParameter:
+      vm_name: '<%= customOptions.vm_name %>'
+      name: '<%= customOptions.vm_name %>'
+      machine_series: '<%= customOptions.machine_series %>'
+      machine_type_override: '<%= customOptions.machine_type_override %>'
+      vcpu_count: '<%= customOptions.vcpu_count %>'
+      memory_gb: '<%= customOptions.memory_gb %>'
+      disk_type: '<%= customOptions.disk_type %>'
+      disk_size_gb: '<%= customOptions.disk_size_gb %>'
+      boot_image_project: '<%= customOptions.boot_image_project %>'
+      boot_image_family: '<%= customOptions.boot_image_family %>'
+      assign_external_ip: '<%= customOptions.assign_external_ip %>'
+      ssh_username: '<%= customOptions.ssh_username %>'
+      ssh_public_key: '<%= customOptions.ssh_public_key %>'
+      user_groups: '<%= customOptions.user_groups %>'
+      network_name: '<%= customOptions.network_name %>'
+      subnetwork_name: '<%= customOptions.subnetwork_name %>'
+      allowed_http_cidr: '<%= customOptions.allowed_http_cidr %>'
+      allowed_ssh_cidr: '<%= customOptions.allowed_ssh_cidr %>'
   EOT
   option_type_ids = local.vm_nginx_option_type_ids
 
