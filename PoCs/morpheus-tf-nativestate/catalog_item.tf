ffb533b020f0b7e29676f0e899d16dc4620775c1
Sandro Cicero <devopsvanilla@outlook.com>
2026-08-23 03:54:08 -0300

    fix(morpheus): declare REMOVE_VM_SCRIPT variable in template

--- PoCs/morpheus-tf-nativestate/catalog_item.tf
+++ PoCs/morpheus-tf-nativestate/catalog_item.tf
@@ -8,11 +8,30 @@ resource "hpe_morpheus_catalog_item_app_blueprint" "vm_nginx" {
 
   blueprint_id    = tonumber(hpe_morpheus_app_blueprint_terraform.vm_nginx.id)
   app_spec        = <<-EOT
-    name: <%= customOptions.vm_name %>
+    name: '<%= customOptions.vm_name %>'
     group:
       id: ${var.morpheus_group_id}
     cloud:
       id: ${var.morpheus_cloud_id}
+    config:
+      name: '<%= customOptions.vm_name %>'
+      vm_name: '<%= customOptions.vm_name %>'
+      machine_series: '<%= customOptions.machine_series %>'
+      machine_type_override: '<%= customOptions.machine_type_override %>'
+      vcpu_count: '<%= customOptions.vcpu_count %>'
+      memory_gb: '<%= customOptions.memory_gb %>'
+      disk_type: '<%= customOptions.disk_type %>'
+      disk_size_gb: '<%= customOptions.disk_size_gb %>'
+      boot_image_project: '<%= customOptions.boot_image_project %>'
+      boot_image_family: '<%= customOptions.boot_image_family %>'
+      assign_external_ip: '<%= customOptions.assign_external_ip %>'
+      ssh_username: '<%= customOptions.ssh_username %>'
+      ssh_public_key: '<%= customOptions.ssh_public_key %>'
+      user_groups: '<%= customOptions.user_groups %>'
+      network_name: '<%= customOptions.network_name %>'
+      subnetwork_name: '<%= customOptions.subnetwork_name %>'
+      allowed_http_cidr: '<%= customOptions.allowed_http_cidr %>'
+      allowed_ssh_cidr: '<%= customOptions.allowed_ssh_cidr %>'
   EOT
   option_type_ids = local.vm_nginx_option_type_ids
 
