920635dce128555b9ab3fc139ded0670bfe85a14
Sandro Cicero <devopsvanilla@outlook.com>
2026-08-23 19:35:55 -0300

    fix: correctly interpolate  into spec_content for google provider credentials

--- PoCs/morpheus-tf-nativestate/spec_template.tf
+++ PoCs/morpheus-tf-nativestate/spec_template.tf
@@ -33,7 +33,7 @@ provider "google" {
   project     = "${var.project_id}"
   region      = "${var.region}"
   zone        = "${var.zone}"
-  credentials = jsonencode(jsondecode(var.gcp_credentials))
+  credentials = ${jsonencode(var.gcp_credentials)}
 }
 
 locals {
