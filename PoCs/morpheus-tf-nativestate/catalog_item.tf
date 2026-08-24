0f04f4e7315808c6c5a36dc330c451455dd76c40
Sandro Cicero <devopsvanilla@outlook.com>
2026-08-24 07:51:19 -0300

    feat: add Morpheus catalog item resource for native Terraform app blueprint

--- PoCs/morpheus-tf-nativestate/catalog_item.tf
+++ PoCs/morpheus-tf-nativestate/catalog_item.tf
@@ -10,7 +10,7 @@ resource "hpe_morpheus_catalog_item_app_blueprint" "vm_nginx" {
   option_type_ids = local.vm_nginx_option_type_ids
 
   app_spec = <<-EOT
-    name: "$${customOptions.name}"
+    name: ${var.blueprint_name}
     group:
       id: ${var.morpheus_group_id}
     cloud:
