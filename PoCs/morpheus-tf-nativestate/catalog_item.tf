91c966ff2968dc9a8670feffaacd69ac0739718d
Sandro Cicero <devopsvanilla@outlook.com>
2026-08-24 19:46:19 -0300

    feat: add Morpheus catalog item resource for native Terraform app blueprint provisioning

--- PoCs/morpheus-tf-nativestate/catalog_item.tf
+++ PoCs/morpheus-tf-nativestate/catalog_item.tf
@@ -15,6 +15,24 @@ resource "hpe_morpheus_catalog_item_app_blueprint" "vm_nginx" {
       id: ${var.morpheus_group_id}
     cloud:
       id: ${var.morpheus_cloud_id}
+    config:
+      customOptions:
+        vm_name: <%=customOptions.vm_name%>
+        machine_series: <%=customOptions.machine_series%>
+        machine_type_override: <%=customOptions.machine_type_override%>
+        vcpu_count: <%=customOptions.vcpu_count%>
+        memory_gb: <%=customOptions.memory_gb%>
+        disk_type: <%=customOptions.disk_type%>
+        disk_size_gb: <%=customOptions.disk_size_gb%>
+        boot_image_project: <%=customOptions.boot_image_project%>
+        boot_image_family: <%=customOptions.boot_image_family%>
+        assign_external_ip: <%=customOptions.assign_external_ip%>
+        ssh_username: <%=customOptions.ssh_username%>
+        ssh_public_key: <%=customOptions.ssh_public_key%>
+        network_name: <%=customOptions.network_name%>
+        subnetwork_name: <%=customOptions.subnetwork_name%>
+        allowed_http_cidr: <%=customOptions.allowed_http_cidr%>
+        allowed_ssh_cidr: <%=customOptions.allowed_ssh_cidr%>
   EOT
 
   content = <<-EOT
